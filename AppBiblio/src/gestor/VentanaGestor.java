2ff1f3c9bd623bf886b595b2d10aae6d1493ccea
AyoubMO04 <ayoubmrg17@gmail.com>
2023-05-18 20:04:48 +0200

    paquete de gestor con interfaz gráfica

---
+++ AppBiblio/src/gestor/VentanaGestor.java
@@ -0,0 +1,128 @@
+package gestor;
+
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class VentanaGestor extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaGestor frame = new VentanaGestor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaGestor() {
+		setTitle("Gestor");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 459, 323);
+		contentPane = new JPanel();
+		contentPane.setForeground(UIManager.getColor("Panel.foreground"));
+		contentPane.setBackground(SystemColor.window);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+		
+		//Creación de panel Titulo
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.window);
+		panel.setForeground(new Color(0, 0, 0));
+		panel.setBorder(new CompoundBorder(null, new LineBorder(new Color(0, 0, 0), 3)));
+		panel.setBounds(53, 14, 359, 44);
+		contentPane.add(panel);
+		
+		//Titulo en el panel
+		JLabel lblGestores = new JLabel("GESTOR");
+		lblGestores.setForeground(new Color(0, 0, 0));
+		lblGestores.setFont(new Font("Dialog", Font.BOLD, 20));
+		panel.add(lblGestores);
+		
+		//Panel de contenido
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setForeground(new Color(0, 0, 0));
+		panel_1.setBackground(SystemColor.window);
+		panel_1.setBorder(new TitledBorder(new CompoundBorder(null, new LineBorder(new Color(0, 0, 0), 3, true)), "Bienvenido", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBounds(53, 70, 360, 208);
+		contentPane.add(panel_1);
+		
+		//Ttitulo del panel de contenido
+		JLabel lblSelecciona = new JLabel("Selecciona una funcion");
+		lblSelecciona.setBackground(SystemColor.activeCaption);
+		lblSelecciona.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblSelecciona.setForeground(new Color(0, 0, 0));
+		lblSelecciona.setBounds(65, 33, 272, 36);
+		panel_1.add(lblSelecciona);
+		
+		
+		//Selección de la función a realizar
+		JComboBox comboBox = new JComboBox();
+		comboBox.setForeground(new Color(238, 238, 236));
+		comboBox.setBackground(new Color(46, 52, 54));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Alta Documento", "Baja Documento", "Modificar Documento", "Copia de seguridad", "Restauracion"}));
+		comboBox.setToolTipText("");
+		comboBox.setBounds(101, 92, 165, 26);
+		panel_1.add(comboBox);
+		
+		//Boton para avanzar
+		JButton btnNewButton = new JButton("Siguiente");
+		btnNewButton.setBounds(110, 161, 151, 35);
+		panel_1.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+
+			//función para cambiar de ventana haciendo click en el boton
+			public void actionPerformed(ActionEvent e) {    
+				//extracción de la funcion seleccionada
+				String seleccion = comboBox.getSelectedItem().toString();
+				if (seleccion == "Alta Documento") {
+					VentanaAltaDocumento frame = new VentanaAltaDocumento();
+					frame.setVisible(true);
+					dispose();
+				} else if(seleccion == "Baja Documento"){
+					VentanaBajaDocumento frame = new VentanaBajaDocumento();
+					frame.setVisible(true);
+					dispose();
+				} else if(seleccion == "Modificar Documento") {
+					VentanaModificarDocumento1 frame = new VentanaModificarDocumento1();
+					frame.setVisible(true);
+					dispose();
+				} else if(seleccion == "Copia de seguridad"){
+					VentanaCopiaSeguridad frame = new VentanaCopiaSeguridad();
+					frame.setVisible(true);
+					dispose();
+				} else {
+					VentanaRestauracion frame = new VentanaRestauracion();
+					frame.setVisible(true);
+					dispose();
+				}
+			}
+		});
+		btnNewButton.setFocusPainted(false);
+		btnNewButton.setBorderPainted(false);
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 14));
+		btnNewButton.setBackground(UIManager.getColor("CheckBoxMenuItem.foreground"));
+	}
+}
---
+++ AppBiblio/src/gestor/VentanaModificarDocumento1.java
@@ -0,0 +1,126 @@
+package gestor;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+
+public class VentanaModificarDocumento1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaModificarDocumento1 frame = new VentanaModificarDocumento1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaModificarDocumento1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 459, 323);
+		contentPane = new JPanel();
+		contentPane.setForeground(UIManager.getColor("Panel.foreground"));
+		contentPane.setBackground(SystemColor.window);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.window);
+		panel.setForeground(new Color(0, 0, 0));
+		panel.setBorder(new CompoundBorder(null, new LineBorder(new Color(0, 0, 0), 3)));
+		panel.setBounds(53, 14, 359, 44);
+		contentPane.add(panel);
+		
+		JLabel lblAlta = new JLabel("MODIFICAR DOCUMENTO");
+		lblAlta.setForeground(new Color(0, 0, 0));
+		lblAlta.setFont(new Font("Dialog", Font.BOLD, 20));
+		panel.add(lblAlta);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setForeground(new Color(238, 238, 236));
+		panel_1.setBackground(SystemColor.window);
+		panel_1.setBorder(new TitledBorder(new CompoundBorder(null, new LineBorder(new Color(0, 0, 0), 3, true)), "Verificaci\u00F3n de la existencia", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBounds(53, 70, 360, 208);
+		contentPane.add(panel_1);
+		
+		JLabel lblIsbn = new JLabel("isbn");
+		lblIsbn.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblIsbn.setBounds(93, 99, 60, 17);
+		panel_1.add(lblIsbn);
+
+		JLabel lblIntroduceElIsbn = new JLabel("Introduce el ISBN");
+		lblIntroduceElIsbn.setForeground(new Color(0, 0, 0));
+		lblIntroduceElIsbn.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblIntroduceElIsbn.setBounds(93, 41, 212, 28);
+		panel_1.add(lblIntroduceElIsbn);
+		
+		textField = new JTextField();
+		textField.setBounds(137, 98, 133, 21);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Modificar\n");
+		btnNewButton.setBounds(257, 168, 91, 28);
+		panel_1.add(btnNewButton);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VentanaModificarDocumento2 frame = new VentanaModificarDocumento2();
+				frame.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setFocusPainted(false);
+		btnNewButton.setBorderPainted(false);
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 12));
+		btnNewButton.setBackground(UIManager.getColor("Button.darkShadow"));
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VentanaGestor frame = new VentanaGestor();
+				frame.setVisible(true);
+				dispose();
+			}
+		});
+		btnVolver.setForeground(Color.BLACK);
+		btnVolver.setFont(new Font("Dialog", Font.BOLD, 12));
+		btnVolver.setFocusPainted(false);
+		btnVolver.setBorderPainted(false);
+		btnVolver.setBackground(UIManager.getColor("Button.darkShadow"));
+		btnVolver.setBounds(12, 168, 79, 28);
+		panel_1.add(btnVolver);
+
+	}
+
+}
