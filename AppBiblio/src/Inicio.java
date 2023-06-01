17bff39ab80e9e048e534b3f9152a21ea80dcdcd
Miguel <migayz2002@gmail.com>
2023-06-01 03:17:38 +0200

    ...

---
+++ AppBiblio/src/Inicio.java
@@ -0,0 +1,10 @@
+import gui.VentanaInicioSesion;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		VentanaInicioSesion app = new VentanaInicioSesion();
+		app.setVisible(true);
+	}
+
+}
